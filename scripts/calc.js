77399d2f34c6be20f080551fda17c5e6d40043bb
Henry Kulikov <henrikas.kulikov@gmail.com>
2018-11-18 20:45:16 +0000

    final commit, end of module

--- scripts/calc.js
+++ scripts/calc.js
@@ -3,7 +3,7 @@ Calculator = function() {
 }
 
 Calculator.prototype.add = function(number) {
-    if(typeof(numer) == "number") {
+    if (typeof(number) == "number") {
         this.value += number;
     } else {
         alert("Error!");
